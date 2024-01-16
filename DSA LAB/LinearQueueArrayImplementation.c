3479847824d1b8087b5f512b007f410c98411cf8
CodeOrganizer <irakib3698@gmail.com>
2024-01-17 02:01:45 +0530

    linear_Queue_Array_Implementation

--- DSA LAB/LinearQueueArrayImplementation.c
+++ DSA LAB/LinearQueueArrayImplementation.c
@@ -1,2 +1,62 @@
 #include<stdio.h>
-#
+#define MAX 3
+int queue[MAX],front=-1,rear=-1;
+
+void queue_insert(int queue[],int val){
+    if(rear==MAX-1)
+        printf("\nQueue Overflow");
+    else {
+    if(front==-1 && rear==-1)
+        front=rear=0;
+    else
+        rear++;
+    queue[rear]=val;}
+}
+int queue_delete(int queue[]){
+    int x;
+    if(front==-1||front>rear){
+        printf("\nQueue Underflow");
+        return -1;
+    }
+    else{
+        x=queue[front];
+        front++;
+        if(front>rear)
+        front=rear=-1;
+        return x;
+    }
+}
+void display(int queue[]){
+    if(front==-1||front>rear)
+        printf("\nQueue is empty");
+    else{
+        for(int i=front;i<=rear;i++)
+        printf("%d ",queue[i]);
+    }
+}
+
+int main(){
+int option,val;
+do{
+    printf("\n*****Main Menu*****");
+    printf("\n1. INSERT");
+    printf("\n2. DELETE");
+    printf("\n3. Display");
+    printf("\n4. EXIT");
+    printf("\nEnter your option: ");
+    scanf("%d",&option);
+        switch(option){
+            case 1: printf("\nEnter the element to be inserted in the queue: ");
+                    scanf("%d",&val);
+                    queue_insert(queue,val);
+                    break;
+            case 2: val=queue_delete(queue);
+                    if(val!=-1)
+                    printf("The element removed from the queue: %d",val);
+                    break;
+
+            case 3: display(queue);
+                    break;
+        }
+}while(option!=4);
+}
