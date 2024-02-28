cd6079eef4d664efb69c030b9b025c4cfb662591
CodeOrganizer <irakib3698@gmail.com>
2024-02-28 13:46:44 +0530

    java 1st

---
+++ JAVA/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.println("\\Hello World\\");
+		System.out.println("\t\"Rakib\"");
+		/*this
+		 * is
+		 * a
+		 * comment.
+		 */
+	}
+
+}
