a107547346b9b84792f27a1d38d92bad51642369
CodeOrganizer <irakib3698@gmail.com>
2024-08-17 12:47:47 +0530

    Updated ooplab

---
+++ OOP Lab/DesignPattern.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class DesignPattern {
+    
+    public static void drawTriangle(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void drawSquare(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void drawRectangle(int m,int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        int a,n,m;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter choice[1. Triangle, 2. Rectangle, 3. Square]: ");
+        a=sc.nextInt();
+        sc.close();
+        switch(a)
+        {
+            case 1: System.out.println("Enter the range: ");
+                    n=sc.nextInt();
+                    DesignPattern.drawTriangle(n);
+                    break;
+            case 3: System.out.println("Enter the range: ");
+                    n=sc.nextInt();
+                    DesignPattern.drawSquare(n);
+                    break;
+            case 2: System.out.println("Enter the range: ");
+                    n=sc.nextInt();
+                    m=sc.nextInt();
+                    DesignPattern.drawRectangle(m, n);
+                    break;
+        }
+    }
+}
---
+++ OOP Lab/Reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Reverse
+{
+    public static void main(String[] args) {
+        String str;int i=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String");
+        str = sc.nextLine();
+        sc.close();
+        for(i=str.length()-1;i>=0;i--)
+           System.out.print(str.charAt(i));
+    }   
+}
+    
